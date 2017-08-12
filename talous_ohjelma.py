1d2448933affd5e9a76b06f7e287af79da22bd4f
aleksi <aleksi.kauramaki@outlook.com>
2017-08-12 20:26:06 +0300

    Updated talous_ohjelma.py

--- talous_ohjelma.py
+++ talous_ohjelma.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-# NOTE! Replace time delay with a promt to press 'enter' when ready to 
-# continue.
-import time
-
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -14,8 +10,8 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-# Used to promt user for input.
-promt = "> "
+# Used to prompt user for input.
+prompt = "> "
 cont = "(Hit RETURN to continue...)"
 
 # Main program runs in a while loop
@@ -23,13 +19,14 @@ while True:
     print("\nWhat do you want to do?\n\t(A) Calculate how much your savings cumulate "
         "over time\n\t(B) Calculate your passive income\n\t(C) Calculate ROE for your "
         "investments\n\t(D) Calculate the future value of money\n\t(E) Calculate the "
-        "present value of money\n\t(X) Exit\n")
-    answer = input("Enter corresponding letter and press enter : ").lower()
+        "present value of money\n\t(X) Exit\n\nEnter corresponding letter and press "
+        "RETURN.")
+    answer = input(prompt).lower()
     if answer == "a":
         print("\nHow much will you save in a year?")
-        amount = int(input(promt))
+        amount = int(input(prompt))
         print("For how many years will you continue to save?")
-        years = int(input(promt))
+        years = int(input(prompt))
         voitto = amount*1.07**5
         for x in range(1, years):
             voitto = voitto+amount*1.07**x
@@ -38,7 +35,7 @@ while True:
         input(cont)
     elif answer == "b":
         print("\nHow much have you invested?")
-        vastaus = int(input(promt))
+        vastaus = int(input(prompt))
         v_tulot = vastaus*0.07*0.98
         kk_tulot = v_tulot/12
         tulot = round(kk_tulot*0.7)
@@ -47,31 +44,31 @@ while True:
         input(cont)
     elif answer == "c":
         print("\nHow much debt?")
-        velka = int(input(promt))
+        velka = int(input(prompt))
         print("What is the the interest (%) for the debt?")
-        velan_korko = float(input(promt))
+        velan_korko = float(input(prompt))
         print("How much capital you have invested?")
-        oma_paaoma = int(input(promt))
+        oma_paaoma = int(input(prompt))
         print("What is the return (%) for the investment?")
-        tuotto = float(input(promt))
+        tuotto = float(input(prompt))
         roe = (((velka + oma_paaoma) * tuotto - (velka * velan_korko)) / 
             oma_paaoma)
         print(bcolors.OKGREEN + "\nYour ROE is %s %%\n" % roe + bcolors.ENDC)
         input(cont)
     elif answer == "d":
         print("\nWhat is the amount of money you have now?")
-        pv = int(input(promt))
+        pv = int(input(prompt))
         print("Amount of years to wait?")
-        yrs = int(input(promt))
+        yrs = int(input(prompt))
         a = round(pv*(1.02**yrs))
         print(bcolors.OKGREEN + "\nThat will grow to %s € in %s years\n" 
             % (a, yrs) + bcolors.ENDC)
         input(cont)
     elif answer == "e":
         print("\nHow much money do you want to have?")
-        fv = int(input(promt))
+        fv = int(input(prompt))
         print("In how many years?")
-        yrs = int(input(promt))
+        yrs = int(input(prompt))
         a = round(fv / (1.0175 ** yrs))
         print(bcolors.OKGREEN + "\nThat equals to %s € in todays money\n" 
             % a + bcolors.ENDC)
