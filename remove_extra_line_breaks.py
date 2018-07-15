3611972d010a16b46f6cc6a4deb0aeec761778fe
aleksi <aleksi.kauramaki@outlook.com>
2018-07-15 12:04:55 +0300

    added copy to clipboard feature

--- remove_extra_line_breaks.py
+++ remove_extra_line_breaks.py
@@ -1,9 +1,12 @@
+import pyperclip
+
 data = ""
-with open("/home/cornucopia/in_file") as file:
+with open("/home/cornucopia/Documents/koodi/in_file") as file:
 	for line in file.readlines():
              data = data.lstrip(" ")
              if line == "\n":
                  data = data + "\n\n"
              else:
                  data = data + " " + line.lstrip(" ").rstrip("\n")
-print(data)
+pyperclip.copy(data)
+print("Text copied to clipboard!")
