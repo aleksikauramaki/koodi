5d60d7e657f288f47aef2fbe6e4f7c2a6dbf035f
aleksi <aleksi.kauramaki@outlook.com>
2018-07-16 21:00:06 +0300

    Refined the remove extra line breaks to perfection.

--- remove_extra_line_breaks.py
+++ remove_extra_line_breaks.py
@@ -1,12 +1,29 @@
 import pyperclip
 
 data = ""
-with open("/home/cornucopia/Documents/koodi/in_file") as file:
-	for line in file.readlines():
-             data = data.lstrip(" ")
-             if line == "\n":
-                 data = data + "\n\n"
-             else:
-                 data = data + " " + line.lstrip(" ").rstrip("\n")
-pyperclip.copy(data)
-print("Text copied to clipboard!")
+
+# Get data from clipboard
+clip = pyperclip.paste().splitlines()
+
+# Remove extra line breaks, but leave empty lines
+for line in clip:
+    data = data.lstrip(" ")
+    if line == "":
+        data += "\n\n"
+    else:
+        data += ' ' + line
+
+# Initialize variables for second round of modification
+data_tmp = data.split("\n")
+data_final = ""
+
+# Remove leading whitespaces from otherwise ready text
+for line in data_tmp:
+    if line == "":
+        data_final += "\n\n"
+    else:
+        data_final += line.lstrip(" ")
+
+# Copy the modified text to clipboard and notify user
+pyperclip.copy(data_final)
+print("Modified data added to clipboard!")
